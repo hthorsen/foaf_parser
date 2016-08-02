532ea7302bafec0fa2167bacee0d6b2aaf4b7648
H. Thorsen <thorsenh@stanford.edu>
2016-08-02 15:02:23 -0700

    adding URL instance variable

--- spec/lib/foaf_parser_spec.rb
+++ spec/lib/foaf_parser_spec.rb
@@ -5,9 +5,23 @@ describe FoafParser do
   it 'instantiates' do
     fp = FoafParser.new()
     expect(fp).to be_kind_of(FoafParser)
+    expect(fp.url).to eq("http://stanford.edu/~sdoljack/sdoljack_foaf.rdf")
   end
+
   it 'introduces itself' do
   	fp = FoafParser.new()
   	expect(fp.speak).to eq("I'm FoafParser!")
   end
+
+  it 'instantiates with rdf' do
+  	fp = FoafParser.new("http://stanford.edu/~bess/foaf.rdf")
+  	expect(fp).to be_kind_of(FoafParser)
+  end
+
+  it 'has URL' do
+  	fp = FoafParser.new("http://stanford.edu/~bess/foaf.rdf")
+  	expect(fp.url).to eq("http://stanford.edu/~bess/foaf.rdf")
+  end
+
+
 end
